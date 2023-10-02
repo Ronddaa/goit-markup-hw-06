e6c3975835ce00e0bec1a80a49c163dd3031c650
Ronddaa <tronddaa@gmail.com>
2023-10-02 15:07:57 +0200

    Start Burger Menu Is Open

--- js/js.js
+++ js/js.js
@@ -5,3 +5,7 @@ document.getElementById("is-open").addEventListener("click",function () {
 document.getElementById("close-modal-window").addEventListener("click",function () {
     document.getElementById("modal-is-open").classList.remove("is-open")
 })
+
+document.getElementById("is-open-burger").addEventListener("click",function () {
+    document.getElementById("is-open-burger-menu").classList.add("is-open")
+})
