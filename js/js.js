2690a95975c575949b82fab5a132f6b3a8965b30
Ronddaa <tronddaa@gmail.com>
2023-10-03 15:07:50 +0200

    Finish Burger Menu JS

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 // Modal Window - Open / Close
 
-document.getElementById("is-open-modal").addEventListener("click", function () {
+document.getElementById("is-open-modal").addEventListener("click",function () {
     document.getElementById("modal-is-open").classList.add("is-open")
 })
 
@@ -10,10 +10,10 @@ document.getElementById("close-modal-window").addEventListener("click",function 
 
 // Burger Menu - Open / Close
 
-document.getElementById("is-open-burger").addEventListener("click", function () {
+document.getElementById("is-open-burger").addEventListener("click",function () {
     document.getElementById("burger-is-open").classList.add("is-open")
 })
 
-document.getElementById("is-open-burger").addEventListener("click", function () {
+document.getElementById("close-burger-menu").addEventListener("click",function () {
     document.getElementById("burger-is-open").classList.remove("is-open")
 })
